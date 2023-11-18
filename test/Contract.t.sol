f32392efc99511dfd5fda403f872abab3026c4b0
laukkw <1319339638@qq.com>
2023-11-18 23:45:20 +0800

    init

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,23 +1,6 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
-
-import "forge-std/Test.sol";
-
-import "src/Contract.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+import {Test} from "forge-std/Test.sol";
 
 contract TestContract is Test {
-    Contract c;
-
-    function setUp() public {
-        c = new Contract();
-    }
-
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
-
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
-    }
 }
